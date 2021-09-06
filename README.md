cc3fe81d71ad8112f27962a67b8a6a29f45ab40a
Jordan Scott <jscott0751@gmail.com>
2021-09-05 21:19:28 -0400

    readMe generating, need to fix indentation

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# readMe-generator
+hi
+
+        ## License Badge
+        
+        ## Description 
+        
+        hi
+        
+        ## Table of Contents 
+        
+        - # installation
+                
+        - # Usage 
+        
+        - # License
+        
+        - # Contributing
+        
+        - # Tests
+        
+        - # Questions
+        
+        ## Installation 
+        
+        jhi
+        
+        ## Usage
+        
+        hi
+        
+        ## License 
+        
+        This project is leased under the Eclipse license
+        
+        ## Contributing 
+        
+        hi
+        
+        ## Tests 
+        
+        undefined
+        
+        ## Questions
+
+        If you have anu questions reach me at hi or you can find some of my work at hi.
+        
--- index.js
+++ index.js
@@ -6,44 +6,113 @@ inquirer
 .prompt([
     {
         type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
         name: 'name',
-        message: 'What is the title?'
+        message: 'What is your project name?'
     },
     {
         type: 'input',
-        name: 'installation-instrcutions',
-        message: 'What are your installation instructions?'
-    },
-    {
-        type: 'input',
-        name: 'usage-information',
-        message: 'What is your usage information?'
-    },
-    {
-        type: 'input',
-        name: 'contribution-guidelines',
-        message: 'What are your contribution guidelines?'
-    },
-    {
-        type: 'input',
-        name: 'test-instructions',
-        message: 'What are your test instructions?'
+        name: 'description',
+        message: 'Write a description of this project'
     },
     {
         type: 'list',
         name: 'license',
         message: 'What license is the application covered under?',
-        choices: ['Eclipse', 'MIT', 'Mozilla'],
-
-    }
-    
+        choices: ['EPL', 'MIT', 'Mozilla'],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command do you run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command do you run to run tests?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using this repo?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?'
+    },
 ])
+.then((data) => {
+     console.log(data);
+    var readMe = 
+    
+        `${data.name}
 
-// TODO: Create a function to write README file
+        ## ${}
+        
+        ## Description 
+        
+        ${data.description}
+        
+        ## Table of Contents 
+        
+        - # installation
+                
+        - # Usage 
+        
+        - # License
+        
+        - # Contributing
+        
+        - # Tests
+        
+        - # Questions
+        
+        ## Installation 
+        
+        ${data.installation}
+        
+        ## Usage
+        
+        ${data.usage}
+        
+        ## License 
+        
+        ${data.license}
+        
+        ## Contributing 
+        
+        ${data.contribution}
+        
+        ## Tests 
+        
+        ${data.tests}
+        
+        ## Questions
+
+        If you have anu questions reach me at ${data.email} or you can find some of my work at ${data.github}.
+        `
+
+    fs.writeFile('readMe.md', readMe, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+});
+
+
+// // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
